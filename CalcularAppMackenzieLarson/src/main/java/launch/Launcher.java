df9fbafc1e29e3435cdb4b314080f55ac6d6a80f
macklarson <mlarson28@mail.greenriver.edu>
2017-10-06 13:51:26 -0700

    Added Javadocs

--- CalcularAppMackenzieLarson/src/main/java/launch/Launcher.java
+++ CalcularAppMackenzieLarson/src/main/java/launch/Launcher.java
@@ -1,10 +1,27 @@
+/*
+ * Mackenzie Larson
+ * 10/6/2017
+ * Calculator App Part 1
+ * Launcher.java
+ */
+
 package launch;
 
 import javafx.application.Application;
 import ui.CalculatorUI;
 
+/**
+ * @author Mackenzie Larson
+ * @version 1.0
+ *
+ * Runs the classes for the calculator applicaton
+ */
 public class Launcher
 {
+    /**
+     * Launches the CalculatorUI class
+     * @param args
+     */
     public static void main(String[] args)
     {
         Application.launch(CalculatorUI.class, args);
